99e7070e475694c59137757cf6ca6f4c4b6bd9a0
Karla038 <karladk038@gmail.com>
2024-08-12 17:45:25 -0600

    Configuracion https

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
+const fs = require('fs');
+
 
 
 //Creando el servidor con express
@@ -14,14 +16,24 @@ dbConnection();
 app.use(express.json());
 
 //Cors
-app.use(cors('*'));
+// app.use(cors('*'));
 
 //Rutas
 app.use('/api/auth', require('./routes/auth_route'));
 app.use('/api/tasks', require('./routes/task_route'));
 
+// Leer certificados
+const privateKey = fs.readFileSync('/etc/nginx/ssl/nginx-taskcute.key', 'utf8');
+const certificate = fs.readFileSync('/etc/nginx/ssl/nginx-taskcute.crt', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
 
 //Escuchar peticiones
-app.listen( process.env.PORT, () => {
-    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`)
+// app.listen( process.env.PORT, () => {
+//     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`)
+// });
+
+// Escuchar peticiones con HTTPS
+https.createServer(credentials, app).listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
 });
